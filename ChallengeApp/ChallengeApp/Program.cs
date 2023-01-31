5ed1629748615fca0af1a633d1b1cec2a6bdc777
Tomek <Tomek@DESKTOP-GB20R8L>
2023-01-31 17:28:18 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Zadanie domowe dzień 4
+
+//Jeżeli jesteś kobietą a czujesz się meżczyzną albo odwrotnie ten program nie jest dla ciebie przepraszam :)
+
+var name = "Ewa";
+var gender = "kobieta";
+var age = 23;
+
+Console.WriteLine("\n");
+
+if (gender == "kobieta")
+{
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine(name + ", lat " + age);
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("Masz na imię " + name + " jesteś " + gender + " oraz masz " + age + " lat.");
+    }
+}
+else if (age >= 18)
+{
+    Console.WriteLine("Mężczyzna pełnoletni");
+}
+else
+{
+    Console.WriteLine("Mężczyzna niepełnoletni");
+}
