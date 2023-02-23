fc95eab0be8c4c75857689b08aa1b522b347147b
Tomek <Tomek@DESKTOP-GB20R8L>
2023-02-23 22:44:55 +0100

    Zadanie dzień 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,34 +1,76 @@
-﻿
-using System.Globalization;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using ChallengeApp;
 
+var employee1 = new Employee("Adam", "Nowak", 25);
+Employee employee2 = new Employee("Max", "Kowalski", 35);
+Employee employee3 = new Employee("Ewa", "Ul", 30);
 
-var number =5345346;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
-int[] wynik = new int[10];
-int z = 0;
-foreach (int x in wynik)//zerowanie tablicy
-{
-    wynik[x] = 0;
-}
+employee1.AddScore(1);
+employee1.AddScore(0);
+employee1.AddScore(0);
+employee1.AddScore(0);
+employee1.AddScore(0);
 
-foreach(char letter in letters)
+employee2.AddScore(3);
+employee2.AddScore(0);
+employee2.AddScore(0);
+employee2.AddScore(0);
+employee2.AddScore(0);
+
+employee3.AddScore(3);
+employee3.AddScore(0);
+employee3.AddScore(0);
+employee3.AddScore(0);
+employee3.AddScore(0);
+
+var result1 = employee1.Result;
+var result2 = employee2.Result;
+var result3 = employee3.Result;
+
+Console.WriteLine(result1);
+Console.WriteLine(result2);
+Console.WriteLine(result3);
+
+if (result1 > result2 && result1 > result3)
 {
-    if (letter == '0') { wynik[0]++; }
-    else if (letter == '1') { wynik[1]++;}
-    else if (letter == '2') { wynik[2]++; }
-    else if (letter == '3') { wynik[3]++; }
-    else if (letter == '4') { wynik[4]++; }
-    else if (letter == '5') { wynik[5]++; }
-    else if (letter == '6') { wynik[6]++; }
-    else if (letter == '7') { wynik[7]++; }
-    else if (letter == '8') { wynik[8]++; }
-    else    { wynik[9]++; }
+    Console.WriteLine("Najwyższa ocena pracownika to: " + result1);
 }
-Console.WriteLine("Liczba w zadaniu to: "+number+"\n");
-foreach(int i in wynik)
+else if (result2 > result3 && result2 > result1)
 {
-    Console.WriteLine(z+" występuje: "+i + " razy");
-    z++;
+    Console.WriteLine("Najwyższa ocena pracownika to: " + result2);
 }
+else if (result3 > result1 && result3 > result2)
+{
+    Console.WriteLine("Najwyższa ocena pracownika to: " + result3);
+}
+else if (result3 == result1 && result3 != result2)
+{
+    Console.WriteLine("Najwyższe oceny dwuch pracowników są równe, wynoszą. " + result3);
+}
+else if (result2 == result1 && result1 != result3)
+{
+    Console.WriteLine("Najwyższe oceny dwuch pracowników są równe, wynoszą. " + result1);
+}
+else if (result2 == result3 && result2 != result1)
+{
+    Console.WriteLine("Najwyższe oceny dwuch pracowników są równe, wynoszą. " + result2);
+}
+else
+{
+    Console.WriteLine("Oceny pracowników są równe."+result1);
+}
+//ROZWIAZANIE ZE WSKAZÓWKA
+List<Employee> employees = new List<Employee>()
+{
+    employee1, employee2, employee3
+};
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+foreach (Employee employee in employees)
+{
+    if(employee.Result>maxResult)
+    {
+        employeeWithMaxResult = employee;
+        maxResult=employee.Result;
+    }
+}
+Console.WriteLine("Najwyższa ocena pracownika to: " + maxResult);
