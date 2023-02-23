27f754c5602ae3b5143a6f331b96f22e5bbdf1a8
Tomek <Tomek@DESKTOP-GB20R8L>
2023-02-23 22:51:44 +0100

    Zadanie dzień 6

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,5 +1,4 @@
-﻿
-namespace ChallengeApp
+﻿namespace ChallengeApp
 {
     public class Employee
     {
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -5,22 +5,22 @@ Employee employee2 = new Employee("Max", "Kowalski", 35);
 Employee employee3 = new Employee("Ewa", "Ul", 30);
 
 employee1.AddScore(1);
-employee1.AddScore(0);
-employee1.AddScore(0);
-employee1.AddScore(0);
-employee1.AddScore(0);
+employee1.AddScore(2);
+employee1.AddScore(3);
+employee1.AddScore(4);
+employee1.AddScore(5);
 
+employee2.AddScore(2);
 employee2.AddScore(3);
-employee2.AddScore(0);
-employee2.AddScore(0);
-employee2.AddScore(0);
-employee2.AddScore(0);
+employee2.AddScore(4);
+employee2.AddScore(5);
+employee2.AddScore(6);
 
 employee3.AddScore(3);
-employee3.AddScore(0);
-employee3.AddScore(0);
-employee3.AddScore(0);
-employee3.AddScore(0);
+employee3.AddScore(4);
+employee3.AddScore(5);
+employee3.AddScore(6);
+employee3.AddScore(7);
 
 var result1 = employee1.Result;
 var result2 = employee2.Result;
